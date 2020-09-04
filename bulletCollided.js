cf3c373f34dff904335e9a4955f3579f91b08892
koolkid007 <70420251+koolkid007@users.noreply.github.com>
2020-09-03 17:15:44 -0700

    Add files via upload

---
+++ bulletCollided.js
@@ -0,0 +1,10 @@
+function hasCollided() {
+  bulletRightEdge = bullet.x + bullet.width;
+  wallLeftEdge = wall.x;
+
+  if(bulletRightEdge >= wallLeftEdge) {
+      return true;
+  }
+      return false;
+
+}
